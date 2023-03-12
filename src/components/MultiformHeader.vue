c286751a57477149baac41f81a42812d80c94a45
MichealAdeotun <adedotun2096@gmail.com>
2023-03-12 11:02:50 +0100

    made changes with styles, added router-view

--- src/components/MultiformHeader.vue
+++ src/components/MultiformHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="w-screen h-screen overflow-hidden bg-Pastel-blue md:flex md:justify-center md:items-center">
-     <section class=" flex w-full h-1/3 md:bg-white md:rounded-xl md:w-2/3 md:h-2/3 md:p-3">
+     <section class=" flex relative w-full h-1/3 md:bg-white md:rounded-xl md:w-2/3 md:h-2/3 md:p-3">
        <section class="
                   bg-sidebar-mobile bg-no-repeat bg-cover w-full h-full p-6
                   md:bg-sidebar-desktop md:bg-no-repeat md:h-full md: md:bg-cover md:w-1/3 md:rounded-lg"
@@ -8,7 +8,7 @@
          <div>
            <div class="text-white text-xl flex justify-center items-center text-lg md:hidden">
              <router-link to="/"><span class="mr-2 border border-white py-2 px-4 rounded-full font-semibold hover:bg-Light-blue hover:text-Marine-blue">1</span></router-link>
-             <router-link to=""><span class="mx-2 border border-white py-2 px-4 rounded-full font-semibold hover:bg-Light-blue hover:text-Marine-blue">2</span></router-link>
+             <router-link to="/plan"><span class="mx-2 border border-white py-2 px-4 rounded-full font-semibold hover:bg-Light-blue hover:text-Marine-blue">2</span></router-link>
              <router-link to=""><span class="mx-2 border border-white py-2 px-4 rounded-full font-semibold hover:bg-Light-blue hover:text-Marine-blue">3</span></router-link>
              <router-link to=""><span class="ml-2 border border-white py-2 px-4 rounded-full font-semibold hover:bg-Light-blue hover:text-Marine-blue">4</span></router-link>
            </div>
@@ -45,14 +45,16 @@
          </div>
        </section>
 
-       <section class="w-2/3 border-2 border-red-300 py-5 pl-10 pr-5">
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, officiis!</p>
+       <section class="md:w-2/3 md:border-2 md:border-red-300 md:py-5 md:px-10">
+         <router-view />
        </section>
      </section>
     </section>
 </template>
 
 <script>
+
+
 
 export default {
   name: "MultiformHeader",
